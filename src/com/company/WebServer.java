cfdee85ab596206e15aad0653297da0698b924a9
raulnegrau <raulnegrau@yahoo.com>
2021-11-06 06:46:30 +0200

    Added necessary files

---
+++ src/com/company/WebServer.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class WebServer {
+    // 0 stopped, 1 running, 2 maintenance
+    private int state;
+    private int portNumber;
+
+    public WebServer () {
+        state = 0;
+        portNumber = 0;
+    }
+
+    public WebServer(int state) {
+        this.state = state;
+    }
+
+    public WebServer(int state, int portNumber) {
+        this.state = state;
+        this.portNumber = portNumber;
+    }
+
+    public void setState (int state) {
+        this.state = state;
+    }
+
+    public int getState () {
+        return state;
+    }
+
+    public int getPortNumber() {
+        return portNumber;
+    }
+
+    public void setPortNumber(int portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    public void startServer(){
+        state = 1;
+    }
+}
---
+++ test/testWebServer.java
@@ -0,0 +1,39 @@
+import com.company.WebServer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class testWebServer {
+
+    WebServer webServer;
+
+    @Before
+    public void before () {
+        webServer = new WebServer();
+    }
+
+
+    @Test
+    public void getStateWhenStopped() {
+        assertEquals(0, webServer.getState());
+    }
+
+    @Test
+    public void getStateWhenRunning() {
+        webServer.startServer();
+        assertEquals(1, webServer.getState());
+    }
+
+    @Test
+    public void getStateWhenInMaintenance () {
+        webServer.setState(2);
+        assertEquals(2, webServer.getState());
+    }
+
+    @Test
+    public void checkWhenThereIsNotAnyPortNumber() {
+        assertEquals(0, webServer.getPortNumber());
+    }
+
+}
